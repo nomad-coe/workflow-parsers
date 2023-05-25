6f4c01023fb8e580defa4adb11f468e71e95518e
Jose Pizarro <jose.pizarro@physik.hu-berlin.de>
2023-05-25 16:03:47 +0200

    Fixed test XSPECTRA

--- tests/test_quantum_espresso_xspectra.py
+++ tests/test_quantum_espresso_xspectra.py
@@ -72,4 +72,4 @@ def test_1(parser):
     assert sec_spectra.type == 'XANES'
     assert sec_spectra.n_energies == 400
     assert sec_spectra.excitation_energies[22].magnitude == approx(-1.6523701378886513e-18)
-    assert sec_spectra.intensities[22] == approx(5.8321411406262735e-09)
+    assert sec_spectra.intensities[22] == approx(2.905e-5)
