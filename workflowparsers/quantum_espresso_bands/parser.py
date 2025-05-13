6a8f46cebd74ba63e67fb89a38a38f119e7cc079
ndaelman <ndaelman@physik.hu-berlin.de>
2025-05-13 16:02:33 +0200

    - Remove superfluous `get_mainfile_keys` - Remove references to old NOMAD search

--- workflowparsers/quantum_espresso_bands/parser.py
+++ workflowparsers/quantum_espresso_bands/parser.py
@@ -17,7 +17,6 @@
 # limitations under the License.
 #
 import os
-import re
 import glob
 import logging
 import numpy as np
@@ -26,16 +25,11 @@ from typing_extensions import TypeAlias
 
 from nomad.units import ureg
 from nomad.parsing.file_parser.text_parser import TextParser, Quantity
-from nomad.search import search
-from nomad.app.v1.models import MetadataRequired
 from runschema.run import Run, Program
-from runschema.method import Method, Electronic, DFT, XCFunctional
-from runschema.system import System, Atoms
 from runschema.calculation import (
     Calculation,
     BandEnergies,
     BandStructure,
-    BandGapDeprecated,
     EnergyEntry,
 )
 from simulationworkflowschema import SinglePoint
@@ -152,22 +146,6 @@ class QuantumEspressoBandsParser:
 
     def __init__(self):
         self.bands_parser = MainfileParser()
-        self.pwscf_entry_id = None
-
-    def get_mainfile_keys(self, filename: str, decoded_buffer: str) -> bool | list[str]:
-        pw_patt = re.compile(r'Program PWSCF')
-        scf_patt = re.compile(r'Self-consistent Calculation')
-        band_patt = re.compile(r'Band Structure Calculation')
-
-        if pw_patt.search(decoded_buffer):
-            if scf_patt.search(decoded_buffer):
-                return ['bs_analysis_pwscf']
-            elif band_patt.search(decoded_buffer):
-                return ['bs_analysis_bands']
-                # this step is currently not analyzed, but covers some edge cases:
-                # 1. no analysis was performed on the band structure
-                # 2. the symmetry analysis failed
-        return False
 
     def parse(self, filepath, archive, logger):
         """
@@ -191,81 +169,16 @@ class QuantumEspressoBandsParser:
                 'This may lead to missing k-points or even k-path segments.'
             )
 
-        # Search NOMAD for matching SCF entry
-        upload_id = self.archive.metadata.upload_id
-        search_hits_data = search(
-            owner='visible',
-            user_id=self.archive.metadata.main_author.user_id,
-            query={'upload_id': upload_id},
-            required=MetadataRequired(include=['entry_id', 'mainfile']),
-        ).data
-        try:
-            self.pwscf_entry_id = search_hits_data[0]['entry_id']
-            search_hit = self.archive.m_context.load_archive(
-                self.pwscf_entry, upload_id, None
-            )
-        except (IndexError, AttributeError):
-            self.logger.warning(
-                'No matching SCF entry found in the same entry.'
-                'Parsing of the band structure will be incomplete.'
-            )
-            return None
-        # TODO: handle multiple hits
-
         sec_run = Run(program=Program(name='Quantum ESPRESSO BANDS'))
         sec_run.program.version = self.bands_parser.get('program_version')
         self.archive.run.append(sec_run)
 
-        self._create_calculation_section(sec_run, search_hit)
-        self._create_system_section(sec_run)
-        self._create_method_section(sec_run)
+        self._create_calculation_section(sec_run)
 
-    def _create_system_section(self, sec_run):
-        """Create the system section from PWSCF data."""
-        sec_system = System()
-        sec_run.system.append(sec_system)
-
-        sec_atoms = Atoms()
-        sec_system.atoms = sec_atoms
-        sec_atoms.periodic = [True] * 3
-
-        alat = self.pwscf_parser.get('alat')
-        cell = self.pwscf_parser.get('simulation_cell')
-        if alat is not None and cell is not None:
-            sec_atoms.lattice_vectors = cell * alat
-        reciprocal_cell = self.pwscf_parser.get('reciprocal_cell')
-        if reciprocal_cell is not None:
-            if alat is not None:
-                sec_atoms.lattice_vectors_reciprocal = reciprocal_cell * (
-                    2 * np.pi / alat
-                )
-
-    def _create_method_section(self, sec_run):
-        """Create the method section."""
-        sec_method = Method()
-        sec_run.method.append(sec_method)
-
-        sec_electronic = Electronic()
-        sec_method.electronic = sec_electronic
-
-        n_electrons = self.pwscf_parser.get('number_of_electrons')
-        if n_electrons is not None:
-            sec_electronic.n_electrons = n_electrons
-
-        n_bands = self.pwscf_parser.get('number_of_bands')
-        if n_bands is not None:
-            sec_electronic.n_bands = n_bands
-
-    def _create_calculation_section(self, sec_run: Run, search_hit: ArchiveSection):
+    def _create_calculation_section(self, sec_run: Run):
         """Create the calculation section with band structure data."""
         sec_calc = Calculation()
         sec_run.calculation.append(sec_calc)
-
-        # Reference the system and method
-        if sec_run.system:
-            sec_calc.system_ref = sec_run.system[-1]
-        if sec_run.method:
-            sec_calc.method_ref = sec_run.method[-1]
 
         # Extract band structure data
         kpoints = self.bands_parser.get('kpoint', [])
@@ -298,33 +211,6 @@ class QuantumEspressoBandsParser:
 
         sec_band_structure = BandStructure(segment=band_segments, band_gap=[])
         sec_calc.band_structure_electronic.append(sec_band_structure)
-
-        # Add reciprocal cell and alignment energies from reference SCF
-        try:
-            sec_band_structure.reciprocal_cell = search_hit.run.system[
-                    -1
-                ].atoms.lattice_vectors_reciprocal
-        except (IndexError, AttributeError):
-            self.logger.warning(f'No reciprocal lattice found in reference SCF (entry_id: {self.pwscf_entry_id}).')
-
-        try:
-            # TODO: handle multiple band gaps
-            search_hit_bg = search_hit.results.properties.electronic.band_gap[0]
-        except (IndexError, AttributeError):
-            self.logger.error(f'No energy alignment data found in reference SCF (entry_id: {self.pwscf_entry_id}).')
-
-        if (eho := search_hit_bg.energy_highest_occupied) is not None:
-            sec_band_structure.energy_fermi = eho
-            sec_band_gap = BandGapDeprecated(energy_highest_occupied=eho)
-            sec_band_structure.band_gap.append(sec_band_gap)
-        else:
-            self.logger.error(f'No energy alignment data found in reference SCF (entry_id: {self.pwscf_entry_id}).')
-
-        if (elu := search_hit_bg.energy_lowest_unoccupied) is not None:
-            sec_band_gap.energy_lowest_unoccupied = elu
-            sec_band_gap.value = sec_band_gap.energy_highest_occupied - sec_band_gap.energy_lowest_unoccupied
-        else:
-            self.logger.warning(f'No LUMO found in reference SCF (entry_id: {self.pwscf_entry_id}).')
 
     def _extract_reference_energy(self, sec_energy, band_energies=None):
         """
